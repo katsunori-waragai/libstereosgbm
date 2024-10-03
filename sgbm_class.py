eb2107781307cc74357437f93d0a30a5b5132f81
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-03 18:56:57 +0900

    reformat by black

--- sgbm_class.py
+++ sgbm_class.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 import numpy as np
 import cv2
 
+
 @dataclass
 class DisparityCalculator:
     window_size = 3
@@ -11,16 +12,17 @@ class DisparityCalculator:
     opencv_sgbm: cv2.StereoSGBM = field(default=None)
 
     def __post_init__(self):
-        self.opencv_sgbm = cv2.StereoSGBM_create(minDisparity=self.min_disp,
-                                       numDisparities=self.max_disp-self.min_disp,
-                                       blockSize=3,
-                                       P1=8 * 3 * self.window_size ** 2,
-                                       P2=32 * 3 * self.window_size ** 2,
-                                       disp12MaxDiff=1,
-                                       uniquenessRatio=10,
-                                       speckleWindowSize=100,
-                                       speckleRange=32
-                                       )
+        self.opencv_sgbm = cv2.StereoSGBM_create(
+            minDisparity=self.min_disp,
+            numDisparities=self.max_disp - self.min_disp,
+            blockSize=3,
+            P1=8 * 3 * self.window_size**2,
+            P2=32 * 3 * self.window_size**2,
+            disp12MaxDiff=1,
+            uniquenessRatio=10,
+            speckleWindowSize=100,
+            speckleRange=32,
+        )
 
     def calc_by_gray(self, grayL: np.ndarray, grayR: np.ndarray) -> np.ndarray:
         disparity = self.opencv_sgbm.compute(grayL, grayR).astype(np.float32) / 16.0
@@ -35,10 +37,9 @@ class DisparityCalculator:
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
 
-    print('loading images...')
-    bgrL = cv2.imread('test/test-imgs/left/left_motorcycle.png')
-    bgrR = cv2.imread('test/test-imgs/right/right_motorcycle.png')
-
+    print("loading images...")
+    bgrL = cv2.imread("test/test-imgs/left/left_motorcycle.png")
+    bgrR = cv2.imread("test/test-imgs/right/right_motorcycle.png")
 
     # disparity range tuning
     # https://docs.opencv.org/trunk/d2/d85/classcv_1_1StereoSGBM.html
@@ -50,10 +51,15 @@ if __name__ == "__main__":
     disparity = disparity_caluculator.calc_by_brg(bgrL, bgrR)
 
     print("saving disparity as disparity_image_sgbm.txt")
-    np.savetxt("data/disparity_image_sgbm.txt", disparity, fmt = '%3.2f', delimiter = ' ', newline = '\n')
+    np.savetxt(
+        "data/disparity_image_sgbm.txt",
+        disparity,
+        fmt="%3.2f",
+        delimiter=" ",
+        newline="\n",
+    )
 
     # plt.imshow(imgL, 'gray')
-    plt.imshow(disparity, 'gray')
+    plt.imshow(disparity, "gray")
     # plt.imshow('disparity', (disparity - min_disp) / num_disp)
     plt.show()
-
