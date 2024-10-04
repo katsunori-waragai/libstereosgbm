06a817ca5633d8faa65686697b0b6b8cbb532173
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-04 12:17:44 +0900

    no pytorch

--- usb_cam.py
+++ usb_cam.py
@@ -6,37 +6,12 @@ import argparse
 
 import cv2
 import numpy as np
-import torch
 
 import stereosgbm
-
-
-def default_args():
-    args = argparse.Namespace(
-        corr_implementation="reg",
-        corr_levels=2,
-        corr_radius=4,
-        hidden_dims=[128, 128, 128],
-        # left_imgs="test-imgs/left/left*.png",
-        max_disp=192,
-        mixed_precision=False,
-        n_downsample=2,
-        n_gru_layers=3,
-        output_directory="./test-output/",
-        restore_ckpt="./stereoigev/models/sceneflow.pth",
-        # right_imgs="test-imgs/right/right*.png",
-        save_numpy=True,
-        shared_backbone=False,
-        slow_fast_gru=False,
-        valid_iters=32,
-    )
-    return args
-
 
 def resize_image(frame: np.ndarray) -> np.ndarray:
     H, W = frame.shape[:2]
     return cv2.resize(frame, (W // 4, H // 4), interpolation=cv2.INTER_AREA)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="disparity tool for ZED2i camera as usb camera")
@@ -50,28 +25,27 @@ if __name__ == "__main__":
     if calc_disparity:
         window_size = 3
         min_disp = 0
-        max_disp = 320
+        max_disp = 160
 
         disparity_calculator = stereosgbm.DisparityCalculator(window_size=window_size, min_disp=min_disp,
                                                                max_disp=max_disp)
     cap = cv2.VideoCapture(video_num)
     cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-    with torch.no_grad():
-        while True:
-            _, frame = cap.read()
-            frame = resize_image(frame)
-            H, W = frame.shape[:2]
-            half_W = W // 2
-            left = frame[:, :half_W, :]
-            right = frame[:, half_W:, :]
+    while True:
+        _, frame = cap.read()
+        frame = resize_image(frame)
+        H, W = frame.shape[:2]
+        half_W = W // 2
+        left = frame[:, :half_W, :]
+        right = frame[:, half_W:, :]
 
-            cv2.imshow("left and right", frame)
+        cv2.imshow("left and right", frame)
 
-            if calc_disparity:
-                disparity = disparity_calculator.calc_by_bgr(left.copy(), right.copy())
-                disp = np.round(disparity * 256).astype(np.uint16)
-                colored = cv2.applyColorMap(cv2.convertScaleAbs(disp, alpha=0.01), cv2.COLORMAP_JET)
-                cv2.imshow("IGEV", colored)
-            key = cv2.waitKey(100)
-            if key == ord("q"):
-                exit()
+        if calc_disparity:
+            disparity = disparity_calculator.calc_by_bgr(left.copy(), right.copy())
+            disp = np.round(disparity * 256).astype(np.uint16)
+            colored = cv2.applyColorMap(cv2.convertScaleAbs(disp, alpha=0.01), cv2.COLORMAP_JET)
+            cv2.imshow("IGEV", colored)
+        key = cv2.waitKey(100)
+        if key == ord("q"):
+            exit()
