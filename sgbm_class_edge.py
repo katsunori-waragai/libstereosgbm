c6cf48045c70d676b29357c4b7abaae48cf11efa
katsunori-waragai <huyuhiko1128@gmail.com>
2024-10-21 11:59:40 +0900

    added argument to sgbm_class_edge.py

--- sgbm_class_edge.py
+++ sgbm_class_edge.py
@@ -1,36 +1,58 @@
+from pathlib import Path
+import glob
+
 import cv2
 import matplotlib.pyplot as plt
 import skimage
 import numpy as np
 
-from dataclasses import dataclass, field
 
 import stereosgbm
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save_numpy", default=True, help="save output as numpy arrays")
+
+    parser.add_argument(
+        "-l", "--left_imgs", help="path to all first (left) frames", default="test/test-imgs/left/*.png"
+    )
+    parser.add_argument(
+        "-r", "--right_imgs", help="path to all second (right) frames", default="test/test-imgs/right/*.png"
+    )
+
+    parser.add_argument("--output_directory", help="directory to save output", default="./demo-output/")
+    parser.add_argument("--max_disp", type=int, default=320, help="max disp of geometry encoding volume")
+
+    args = parser.parse_args()
+
+    from matplotlib import pyplot as plt
+
     window_size = 3
     min_disp = 0
-    max_disp = 128
+    max_disp = args.max_disp
+    outdir = Path(args.output_directory)
+    outdir.mkdir(exist_ok=True)
 
-    left_imgs = ["test/test-imgs/left/left_motorcycle.png"]
-    right_imgs = ["test/test-imgs/right/right_motorcycle.png"]
+    left_imgs = sorted(glob.glob(args.left_imgs, recursive=True))
+    right_imgs = sorted(glob.glob(args.right_imgs, recursive=True))
 
     edge_disparity_calculator = stereosgbm.EdgeBasedDisparityCalculator(
         window_size=window_size, min_disp=min_disp, max_disp=max_disp
     )
-
-    disparity_calculator = stereosgbm.DisparityCalculator(
-        window_size=window_size, min_disp=min_disp, max_disp=max_disp
-    )
+    left_imgs = sorted(glob.glob(args.left_imgs, recursive=True))
+    right_imgs = sorted(glob.glob(args.right_imgs, recursive=True))
 
     for left_name, right_name in zip(left_imgs, right_imgs):
         left_image = cv2.imread(left_name, cv2.IMREAD_GRAYSCALE)
         right_image = cv2.imread(right_name, cv2.IMREAD_GRAYSCALE)
         edge_disparity = edge_disparity_calculator.predict(left_image, right_image)
-        disparity = disparity_calculator.predict(left_image, right_image)
+        left_name = Path(left_name)
+        stem = left_name.stem.replace("left_", "")
+        oname = outdir / f"edge_disparity_{stem}.npy"
+        print(f"saving disparity as {oname}")
+        np.save(str(oname), edge_disparity)
 
-        plt.subplot(1, 2, 1)
-        plt.imshow(disparity)
-        plt.subplot(1, 2, 2)
         plt.imshow(edge_disparity)
         plt.show()
